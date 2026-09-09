653d2a35a5e1a1c50fcb89ea77d475d91ed24be0
gpeppas <gpeppas@ajpsoft.gr>
2026-09-09 15:33:21 +0300

    add Attachment and PersonalInfo entities with relationships to Teacher

---
+++ src/main/java/gr/aueb/cf/eduapp/model/Teacher.java
@@ -0,0 +1,41 @@
+package gr.aueb.cf.eduapp.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.swing.*;
+import java.util.UUID;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(name = "teachers")
+public class Teacher extends AbstractEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false, updatable = false, columnDefinition = "BINARY(16)")
+    private UUID uuid = UUID.randomUUID();
+
+    @Column(unique = true, nullable = false)
+    private String vat;
+
+    @Column(nullable = false)
+    private String firstname;
+
+    @Column(nullable = false)
+    private String lastname;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "region_id")
+    private Region region;
+
+    @OneToOne(cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+    @JoinColumn(name = "personal_info_id", referencedColumnName = "id")
+    private PersonalInfo personalInfo;
+}
