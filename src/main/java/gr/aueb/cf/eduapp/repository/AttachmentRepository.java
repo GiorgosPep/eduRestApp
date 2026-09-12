385c9862c02d91b8a91a896e119196182a6e8075
gpeppas <gpeppas@ajpsoft.gr>
2026-09-12 18:41:18 +0300

    add repositories for Attachment, Capability, PersonalInfo, Region, and Role entities

---
+++ src/main/java/gr/aueb/cf/eduapp/repository/AttachmentRepository.java
@@ -0,0 +1,8 @@
+package gr.aueb.cf.eduapp.repository;
+
+import gr.aueb.cf.eduapp.model.Attachment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AttachmentRepository extends JpaRepository<Attachment, Long> {
+
+}
