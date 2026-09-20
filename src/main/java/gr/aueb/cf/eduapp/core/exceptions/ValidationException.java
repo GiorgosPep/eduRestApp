28468e0b6faa5c72dfc82f09af70ce5599767440
gpeppas <gpeppas@ajpsoft.gr>
2026-09-20 20:33:20 +0300

    add: enhance error handling with additional exception handlers for validation, file upload, data access, authentication, and access denial

--- src/main/java/gr/aueb/cf/eduapp/core/exceptions/ValidationException.java
+++ src/main/java/gr/aueb/cf/eduapp/core/exceptions/ValidationException.java
@@ -1,7 +1,9 @@
 package gr.aueb.cf.eduapp.core.exceptions;
 
+import lombok.Getter;
 import org.springframework.validation.BindingResult;
 
+@Getter
 public class ValidationException extends AppGenericException {
     private static final String DEFAULT_CODE = "ValidationError";
     private final BindingResult bindingResult;
